bfba86bf2cfe463c7cbd4974114e01010f9b7a6c
Julian Peeters <julianpeeters@gmail.com>
2015-12-20 19:08:04 -0800

    Bump to 0.8.1-SNAPSHOT for dev

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object BuildSettings {
 
   val buildSettings = Defaults.defaultSettings ++ scriptedSettings ++ Seq(
     organization := "com.julianpeeters",
-    version := "0.8.0",
+    version := "0.8.1-SNAPSHOT",
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ywarn-value-discard"),
     scalaVersion := "2.11.7",
     crossScalaVersions := Seq("2.10.5", scalaVersion.value),
