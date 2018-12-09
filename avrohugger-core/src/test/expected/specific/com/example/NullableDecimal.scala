9d0d233e5f281a1c753b78555646eef954b13610
Kostya Golikov <johnysilver7@gmail.com>
2018-12-09 11:51:25 +0100

    Fix specific generator for UNIONs with decimal inside
    When generator decides if there are decimal fields in the schema, it
    traverses all top level fields, but does not jump into UNION type. So if
    there is decimal inside, it's ignored.
    
    Fixes #107

---
+++ avrohugger-core/src/test/expected/specific/com/example/NullableDecimal.scala
@@ -0,0 +1,50 @@
+/** MACHINE-GENERATED FROM AVRO SCHEMA. DO NOT EDIT DIRECTLY */
+package com.example
+
+import scala.annotation.switch
+
+case class NullableDecimal(var ciao: Option[BigDecimal] = None) extends org.apache.avro.specific.SpecificRecordBase {
+  def this() = this(None)
+  def get(field$: Int): AnyRef = {
+    (field$: @switch) match {
+      case 0 => {
+        ciao match {
+          case Some(x) => {
+            val schema = getSchema.getFields().get(field$).schema()
+            val decimalType = schema.getLogicalType().asInstanceOf[org.apache.avro.LogicalTypes.Decimal]
+            val scale = decimalType.getScale()
+            val scaledValue = x.setScale(scale)
+            val bigDecimal = scaledValue.bigDecimal
+            NullableDecimal.decimalConversion.toBytes(bigDecimal, schema, decimalType)
+          }
+          case None => null
+        }
+      }.asInstanceOf[AnyRef]
+      case _ => new org.apache.avro.AvroRuntimeException("Bad index")
+    }
+  }
+  def put(field$: Int, value: Any): Unit = {
+    (field$: @switch) match {
+      case 0 => this.ciao = {
+        value match {
+          case null => None
+          case _ => Some(value match {
+            case (buffer: java.nio.ByteBuffer) => {
+              val schema = getSchema.getFields().get(field$).schema()
+              val decimalType = schema.getLogicalType().asInstanceOf[org.apache.avro.LogicalTypes.Decimal]
+              BigDecimal(NullableDecimal.decimalConversion.fromBytes(buffer, schema, decimalType))
+            }
+          })
+        }
+      }.asInstanceOf[Option[BigDecimal]]
+      case _ => new org.apache.avro.AvroRuntimeException("Bad index")
+    }
+    ()
+  }
+  def getSchema: org.apache.avro.Schema = NullableDecimal.SCHEMA$
+}
+
+object NullableDecimal {
+  val SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"NullableDecimal\",\"namespace\":\"com.example\",\"fields\":[{\"name\":\"ciao\",\"type\":[\"null\",{\"type\":\"bytes\",\"scale\":5,\"precision\":64,\"logicalType\":\"decimal\"}],\"default\":null}]}")
+  val decimalConversion = new org.apache.avro.Conversions.DecimalConversion
+}
