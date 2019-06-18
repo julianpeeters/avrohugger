eff4a28abea8af82cdb4c7d514ade49af01cda35
Plinio Santos <plinio.pantaleao@mobimeo.com>
2019-06-18 11:09:57 +0200

    Migration to AVRO 1.9.0

--- avrohugger-core/src/main/scala/matchers/DefaultValueMatcher.scala
+++ avrohugger-core/src/main/scala/matchers/DefaultValueMatcher.scala
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.{NullNode, ObjectNode, TextNode}
 import treehugger.forest
 import treehuggerDSL._
+import scala.util.Try
 
 import scala.util.Try
 import scala.collection.JavaConverters._
