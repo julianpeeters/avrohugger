653b741a2a036ea17095db3c7dec6b890c2d58a5
Rob Landers <landers.robert@gmail.com>
2019-07-23 11:47:15 +0200

    Add to specific classes

--- avrohugger-core/src/main/scala/format/standard/trees/StandardCaseClassTree.scala
+++ avrohugger-core/src/main/scala/format/standard/trees/StandardCaseClassTree.scala
@@ -18,19 +18,6 @@ import scala.collection.JavaConverters._
 
 object StandardCaseClassTree {
 
-  val RESERVED_WORDS: Set[String] = Set("abstract", "assert", "boolean", "break", "byte", "case", "catch",
-    "char", "class", "const", "continue", "default", "do", "double",
-    "else", "enum", "extends", "false", "final", "finally", "float",
-    "for", "goto", "if", "implements", "import", "instanceof", "int",
-    "interface", "long", "native", "new", "null", "package", "private",
-    "protected", "public", "return", "short", "static", "strictfp",
-    "super", "switch", "synchronized", "this", "throw", "throws",
-    "transient", "true", "try", "void", "volatile", "while")
-
-  def backtick(variable: String): String = s"`$variable`"
-
-  def fieldRenamer(fieldName: String): String = if(RESERVED_WORDS.contains(fieldName)) backtick(fieldName) else if (fieldName.endsWith("_")) backtick(fieldName) else fieldName
-
   def toCaseClassDef(
     classStore: ClassStore,
     namespace: Option[String],
