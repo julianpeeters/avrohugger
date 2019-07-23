bcca7c905121285f2d8d515ddd1595f8446f8148
Rob Landers <landers.robert@gmail.com>
2019-07-23 13:56:18 +0200

    Add scavro tests

--- avrohugger-core/src/test/expected/scavro/example/idl/model/Names.scala
+++ avrohugger-core/src/test/expected/scavro/example/idl/model/Names.scala
@@ -11,7 +11,6 @@ case class Names(public$: String, `ends_with_`: String) extends AvroSerializeabl
   type J = JNames
   override def toAvro: JNames = {
     new JNames(public$, `ends_with_`)
-  }
 }
 
 object Names {
