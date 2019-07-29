c101c2756a25e86d523e3cfe0c756a8c550deb78
Rob Landers <landers.robert@gmail.com>
2019-07-29 17:05:51 +0200

    Update tests

--- avrohugger-core/src/test/expected/standard/example/idl/Names.scala
+++ avrohugger-core/src/test/expected/standard/example/idl/Names.scala
@@ -1,6 +1,6 @@
 /** MACHINE-GENERATED FROM AVRO SCHEMA. DO NOT EDIT DIRECTLY */
 package example.idl
 
-case class Names(private val _$$public: String, `ends_with_`: String) {
-  val `public`: String = _$$public
+case class Names(private val __public: String, `ends_with_`: String) {
+  val `public`: String = __public
 }
