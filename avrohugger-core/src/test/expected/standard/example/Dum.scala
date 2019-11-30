60a1976d5a21f48186b5be374f4e0758858e711c
Julian Peeters <julianpeeters@gmail.com>
2019-11-30 13:43:11 -0800

    Add test for top-level-unions

---
+++ avrohugger-core/src/test/expected/standard/example/Dum.scala
@@ -0,0 +1,8 @@
+/** MACHINE-GENERATED FROM AVRO SCHEMA. DO NOT EDIT DIRECTLY */
+package example
+
+final case class Dum(name: String)
+
+object Dum {
+  val SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"Dum\",\"namespace\":\"example\",\"fields\":[{\"name\":\"name\",\"type\":\"string\"}]}")
+}
