08bef84f23492803c2d04a21ef29666301501f83
dtyger <dtyger@gmail.com>
2021-08-03 17:55:19 +0100

    Fix tests

--- avrohugger-core/src/test/expected/specific/example/logical/LogicalSc.scala
+++ avrohugger-core/src/test/expected/specific/example/logical/LogicalSc.scala
@@ -4,7 +4,7 @@ package example.logical
 import scala.annotation.switch
 
 final case class LogicalSc(var data: BigDecimal, var fxField: BigDecimal, var ts: java.time.Instant, var dt: java.time.LocalDate, var uuid: java.util.UUID, var dataBig: BigDecimal) extends org.apache.avro.specific.SpecificRecordBase {
-  def this() = this(scala.math.BigDecimal(0), java.time.Instant.now, java.time.LocalDate.now, java.util.UUID.randomUUID, scala.math.BigDecimal(0))
+  def this() = this(scala.math.BigDecimal(0), scala.math.BigDecimal(0), java.time.Instant.now, java.time.LocalDate.now, java.util.UUID.randomUUID, scala.math.BigDecimal(0))
   def get(field$: Int): AnyRef = {
     (field$: @switch) match {
       case 0 => {
@@ -16,15 +16,23 @@ final case class LogicalSc(var data: BigDecimal, var fxField: BigDecimal, var ts
         LogicalSc.decimalConversion.toBytes(bigDecimal, schema, decimalType)
       }.asInstanceOf[AnyRef]
       case 1 => {
-        ts.toEpochMilli
+        val schema = getSchema.getFields().get(field$).schema()
+        val decimalType = schema.getLogicalType().asInstanceOf[org.apache.avro.LogicalTypes.Decimal]
+        val scale = decimalType.getScale()
+        val scaledValue = fxField.setScale(scale)
+        val bigDecimal = scaledValue.bigDecimal
+        LogicalSc.decimalConversion.toBytes(bigDecimal, schema, decimalType)
       }.asInstanceOf[AnyRef]
       case 2 => {
-        dt.toEpochDay.toInt
+        ts.toEpochMilli
       }.asInstanceOf[AnyRef]
       case 3 => {
-        uuid.toString
+        dt.toEpochDay.toInt
       }.asInstanceOf[AnyRef]
       case 4 => {
+        uuid.toString
+      }.asInstanceOf[AnyRef]
+      case 5 => {
         val schema = getSchema.getFields().get(field$).schema()
         val decimalType = schema.getLogicalType().asInstanceOf[org.apache.avro.LogicalTypes.Decimal]
         val scale = decimalType.getScale()
@@ -46,28 +54,37 @@ final case class LogicalSc(var data: BigDecimal, var fxField: BigDecimal, var ts
           }
         }
       }.asInstanceOf[BigDecimal]
-      case 1 => this.ts = {
+      case 1 => this.fxField = {
+        value match {
+          case (buffer: java.nio.ByteBuffer) => {
+            val schema = getSchema.getFields().get(field$).schema()
+            val decimalType = schema.getLogicalType().asInstanceOf[org.apache.avro.LogicalTypes.Decimal]
+            BigDecimal(LogicalSc.decimalConversion.fromBytes(buffer, schema, decimalType))
+          }
+        }
+      }.asInstanceOf[BigDecimal]
+      case 2 => this.ts = {
         value match {
           case (l: Long) => {
             java.time.Instant.ofEpochMilli(l)
           }
         }
       }.asInstanceOf[java.time.Instant]
-      case 2 => this.dt = {
+      case 3 => this.dt = {
         value match {
           case (i: Integer) => {
             java.time.LocalDate.ofEpochDay(i.toInt)
           }
         }
       }.asInstanceOf[java.time.LocalDate]
-      case 3 => this.uuid = {
+      case 4 => this.uuid = {
         value match {
           case (chars: java.lang.CharSequence) => {
             java.util.UUID.fromString(chars.toString)
           }
         }
       }.asInstanceOf[java.util.UUID]
-      case 4 => this.dataBig = {
+      case 5 => this.dataBig = {
         value match {
           case (buffer: java.nio.ByteBuffer) => {
             val schema = getSchema.getFields().get(field$).schema()
@@ -84,6 +101,6 @@ final case class LogicalSc(var data: BigDecimal, var fxField: BigDecimal, var ts
 }
 
 object LogicalSc {
-  val SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"LogicalSc\",\"namespace\":\"example.logical\",\"fields\":[{\"name\":\"data\",\"type\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":9,\"scale\":2}},{\"name\":\"ts\",\"type\":{\"type\":\"long\",\"logicalType\":\"timestamp-millis\"}},{\"name\":\"dt\",\"type\":{\"type\":\"int\",\"logicalType\":\"date\"}},{\"name\":\"uuid\",\"type\":{\"type\":\"string\",\"logicalType\":\"uuid\"}},{\"name\":\"dataBig\",\"type\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":20,\"scale\":12}}]}")
+  val SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"LogicalSc\",\"namespace\":\"example.logical\",\"fields\":[{\"name\":\"data\",\"type\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":9,\"scale\":2}},{\"name\":\"fxField\",\"type\":{\"type\":\"fixed\",\"name\":\"fxType\",\"size\":12,\"logicalType\":\"decimal\",\"precision\":5,\"scale\":2}},{\"name\":\"ts\",\"type\":{\"type\":\"long\",\"logicalType\":\"timestamp-millis\"}},{\"name\":\"dt\",\"type\":{\"type\":\"int\",\"logicalType\":\"date\"}},{\"name\":\"uuid\",\"type\":{\"type\":\"string\",\"logicalType\":\"uuid\"}},{\"name\":\"dataBig\",\"type\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":20,\"scale\":12}}]}")
   val decimalConversion = new org.apache.avro.Conversions.DecimalConversion
 }
