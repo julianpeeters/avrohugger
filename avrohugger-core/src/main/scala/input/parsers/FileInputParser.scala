7bdf35a9eb47fbc5dc599e2fcebc9ed1f4696b69
Mikolaj Jakubowski <mkl.jakubowski@gmail.com>
2024-12-31 10:32:59 +0100

    explore each file only once

--- avrohugger-core/src/main/scala/input/parsers/FileInputParser.scala
+++ avrohugger-core/src/main/scala/input/parsers/FileInputParser.scala
@@ -19,6 +19,7 @@ import scala.util.Try
 class FileInputParser {
 
   var processedFiles: Set[String] = Set.empty
+  var processedSchemas: Set[Either[Schema, Protocol]] = Set.empty
 
   def getSchemaOrProtocols(
     infile: File,
@@ -75,66 +76,63 @@ class FileInputParser {
       unUnion(parsed.get) // throw the avro parse exception if Failure
     }
 
-    if (processedFiles.contains(infile.getCanonicalPath)) {
-      List.empty
-    }
-    else {
-      processedFiles += infile.getCanonicalPath
-      infile.getName.split("\\.").last match {
-        case "avro" =>
-          val gdr = new GenericDatumReader[GenericRecord]
-          val dfr = new DataFileReader(infile, gdr)
-          val schemas = unUnion(dfr.getSchema)
-          schemas.map(Left(_))
-        case "avsc" =>
-          val schemas = tryParse(infile, parser)
-          schemas.map(Left(_))
-        case "avpr" =>
-          val protocol = Protocol.parse(infile)
-          List(Right(protocol))
-        case "avdl" =>
-          val idlParser = new Idl(infile, classLoader)
-          val protocol = idlParser.CompilationUnit()
-          /**
-            * IDLs may refer to types imported from another file. When converted
-            * to protocols, the imported types that share the IDL's namespace
-            * cannot be distinguished from types defined within the IDL, yet
-            * should not be generated as subtypes of the IDL's ADT and should
-            * instead be generated in its own namespace. So, strip the protocol
-            * of all imported types and generate them separately.
-            */
-          val importedFiles = IdlImportParser.getImportedFiles(infile, classLoader)
-          val importedSchemaOrProtocols = importedFiles.flatMap { file =>
+    val res = infile.getName.split("\\.").last match {
+      case "avro" =>
+        val gdr = new GenericDatumReader[GenericRecord]
+        val dfr = new DataFileReader(infile, gdr)
+        val schemas = unUnion(dfr.getSchema)
+        schemas.map(Left(_))
+      case "avsc" =>
+        val schemas = tryParse(infile, parser)
+        schemas.map(Left(_))
+      case "avpr" =>
+        val protocol = Protocol.parse(infile)
+        List(Right(protocol))
+      case "avdl" =>
+        val idlParser = new Idl(infile, classLoader)
+        val protocol = idlParser.CompilationUnit()
+        /**
+          * IDLs may refer to types imported from another file. When converted
+          * to protocols, the imported types that share the IDL's namespace
+          * cannot be distinguished from types defined within the IDL, yet
+          * should not be generated as subtypes of the IDL's ADT and should
+          * instead be generated in its own namespace. So, strip the protocol
+          * of all imported types and generate them separately.
+          */
+        val importedFiles = IdlImportParser.getImportedFiles(infile, classLoader)
+        val importedSchemaOrProtocols = importedFiles.flatMap { file =>
+          if (!processedFiles.contains(file.getCanonicalPath)) {
+            processedFiles += file.getCanonicalPath
             val importParser = new Parser() // else attempts to redefine schemas
             getSchemaOrProtocols(file, format, classStore, classLoader, importParser)
+          } else {
+            List()
           }
+        }
 
-          def stripImports(
-            protocol: Protocol,
-            importedSchemaOrProtocols: List[Either[Schema, Protocol]]) = {
-            val imported = importedSchemaOrProtocols.flatMap(avroDef => {
-              avroDef match {
-                case Left(importedSchema) => List(importedSchema)
-                case Right(importedProtocol) => importedProtocol.getTypes().asScala
-              }
-            })
-            val types = protocol.getTypes().asScala.toList
-            val localTypes = imported.foldLeft(types)((remaining, imported) => {
-              remaining.filterNot(remainingType => remainingType == imported)
-            })
-            protocol.setTypes(localTypes.asJava)
-            protocol
+        def stripImports(
+          protocol: Protocol,
+          importedSchemaOrProtocols: Set[Either[Schema, Protocol]]) = {
+          val imported = importedSchemaOrProtocols.flatMap {
+            case Left(importedSchema) => Set(importedSchema)
+            case Right(importedProtocol) => importedProtocol.getTypes().asScala
           }
+          val types = protocol.getTypes().asScala.toSet
+          val localTypes = types -- imported
+          protocol.setTypes(localTypes.asJava)
+          protocol
+        }
 
-          val localProtocol = stripImports(protocol, importedSchemaOrProtocols)
-          // reverse to dependent classes are generated first
-          (Right(localProtocol) +: importedSchemaOrProtocols).reverse
-        case _ =>
-          throw new Exception(
-            """File must end in ".avpr" for protocol files,
-              |".avsc" for plain text json files, ".avdl" for IDL files, or .avro
-              |for binary.""".trim.stripMargin)
-      }
+        val localProtocol = stripImports(protocol, processedSchemas)
+        // reverse to dependent classes are generated first
+        (Right(localProtocol) +: importedSchemaOrProtocols).reverse
+      case _ =>
+        throw new Exception(
+          """File must end in ".avpr" for protocol files,
+            |".avsc" for plain text json files, ".avdl" for IDL files, or .avro
+            |for binary.""".trim.stripMargin)
     }
+    res.foreach(processedSchemas += _)
+    res
   }
 }
