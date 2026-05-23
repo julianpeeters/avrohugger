9c0a87fa9429d129858ebbcd3f71ed0f166649e4
Julian Peeters <julianpeeters@gmail.com>
2026-05-23 15:13:04 -0700

    fmt

--- avrohugger-core/src/main/scala/types/AvroScalaTypes.scala
+++ avrohugger-core/src/main/scala/types/AvroScalaTypes.scala
@@ -14,21 +14,21 @@ case class AvroScalaTypes(
   // complex
   fixed:    AvroScalaFixedType    = ScalaCaseClassWrapper,
   record:   AvroScalaRecordType   = ScalaCaseClass,
-  `enum`:     AvroScalaEnumType   = ScalaEnumeration,
+  `enum`:   AvroScalaEnumType     = ScalaEnumeration,
   union:    AvroScalaUnionType    = OptionEitherShapelessCoproduct,
   array:    AvroScalaArrayType    = ScalaSeq,
   map:      AvroScalaMapType      = ScalaMap,
   protocol: AvroScalaProtocolType = NoTypeGenerated,
   // logical
-  decimal:  AvroScalaDecimalType  = ScalaBigDecimal(None),
-  date:     AvroScalaDateType     = JavaTimeLocalDate,
-  timestampMillis: AvroScalaTimestampMillisType = JavaTimeInstant,
-  timeMillis: AvroScalaTimeMillisType = JavaTimeLocalTime,
-  timeMicros: AvroScalaTimeType = JavaTimeLocalTime,
-  timestampMicros: AvroScalaTimestampType = JavaTimeZonedDateTime,
-  localTimestampMillis: AvroScalaLocalTimestampType = JavaTimeLocalDateTime,
-  localTimestampMicros: AvroScalaLocalTimestampType = JavaTimeLocalDateTime,
-  uuid:     AvroUuidType          = JavaUuid
+  decimal:              AvroScalaDecimalType         = ScalaBigDecimal(None),
+  date:                 AvroScalaDateType            = JavaTimeLocalDate,
+  timestampMillis:      AvroScalaTimestampMillisType = JavaTimeInstant,
+  timeMillis:           AvroScalaTimeMillisType      = JavaTimeLocalTime,
+  timeMicros:           AvroScalaTimeType            = JavaTimeLocalTime,
+  timestampMicros:      AvroScalaTimestampType       = JavaTimeZonedDateTime,
+  localTimestampMillis: AvroScalaLocalTimestampType  = JavaTimeLocalDateTime,
+  localTimestampMicros: AvroScalaLocalTimestampType  = JavaTimeLocalDateTime,
+  uuid:                 AvroUuidType                 = JavaUuid
 )
 
 object AvroScalaTypes {
